543b7c12cd0e3191bb95c9da2bda46923b0104ed
Sebastian Gil <shiuxcode@gmail.com>
2021-07-14 17:56:47 -0500

    Fix build

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const copyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
     filename: "build.js",
   },
   resolve: {
